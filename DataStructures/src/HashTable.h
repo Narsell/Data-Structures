d847ee571cd2dcc01373e90a6e68877c7aa8734d
Daniel <narselldev@gmail.com>
2024-04-11 20:17:26 -0500

    Added documentation

--- DataStructures/src/HashTable.h
+++ DataStructures/src/HashTable.h
@@ -21,8 +21,22 @@ public:
 	HashTable();
 	void Print() const;
 
+	/**
+	 * O(1) time complexity, technically O(n) but almost mathematically impossible to behave like O(n).
+	 * Returns the value from a given key. If the key does not exists, it returns INT_MIN.
+	 */
 	const int Get(const std::string& key) const;
+
+	/**
+	 * O(1) time complexity, technically O(n) but almost mathematically impossible to behave like O(n).
+	 * Sets the value of a key. If the key already exists it will be overwritten with the new value.
+	 */
 	void Set(const std::string& key, const int value);
+
+	/**
+	 * O(n) time complexity.
+	 * Retrieves all keys and returns them in a std::vector.
+	 */
 	std::vector<std::string> Keys() const;
 
 private:
